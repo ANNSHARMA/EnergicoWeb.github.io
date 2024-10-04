e2106045a63ab94c981b7cc893f69afcaf01e741
Anmol sharma <as7067647@gmail.com>
2024-10-04 19:29:18 +0530

    add js or tailwindcss folder

---
+++ toggle.js
@@ -0,0 +1,9 @@
+let togg = document.querySelector('.bars');
+let text = document.querySelector('.links')
+// console.log(bars);
+// console.log(link);
+
+
+togg.addEventListener('click', function(){
+
+})
